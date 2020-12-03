87f738b69c17cc6cc8070f81757c07654e1f2ad5
milad-mirkarimi <themiladmirkarimi@gmail.com>
2020-12-03 19:37:44 +1300

    register github route

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.get('/', (req, res) => res.send('API RUNNING!'));
 
+app.use('/api/github', require('./routes/api/github'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
