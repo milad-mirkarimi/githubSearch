1b97ac51f140ae42c264e11841ec9d94fee48a2f
milad-mirkarimi <themiladmirkarimi@gmail.com>
2020-12-04 23:30:42 +1300

    change github search user path

--- routes/api/github.js
+++ routes/api/github.js
@@ -9,8 +9,8 @@ const config = require('config');
 router.get('/users/:username', (req, res) => {
     try {
         const options = {
-            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5
-            &sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+            uri: `https://api.github.com/search/users?q=${req.params.username}&per_page=10
+            &client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
             method: 'GET',
             headers: { 'user-agent': 'node.js' }
         };
