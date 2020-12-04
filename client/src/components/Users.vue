d5482e989ab273b0ad79e44281e8bcd3ecd578f0
milad-mirkarimi <themiladmirkarimi@gmail.com>
2020-12-05 08:06:53 +1300

    add basic SearchControl template

--- client/src/components/Users.vue
+++ client/src/components/Users.vue
@@ -2,14 +2,17 @@
   <div class="row">
     <div class="column">
       <h1>Search more than 63M users</h1>
+      <SearchControl />
     </div>
   </div>
 </template>
 
 <script>
-
-
+import SearchControl from "@/components/shared/controls/SearchControl.vue";
 export default {
   name: "Users",
+  components: {
+    SearchControl
+  }
 };
 </script>
---
+++ client/src/components/shared/controls/SearchControl.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="search-control">
+		<input type="search">
+		<button>Search</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "SearchControl",
+}
+</script>
