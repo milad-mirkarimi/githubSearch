e62d81aa1f254935ec5814d715440b88117d71bb
milad-mirkarimi <themiladmirkarimi@gmail.com>
2020-12-06 09:51:46 +1300

    add click outside directive

--- client/src/components/shared/controls/sortControl/SortControl.vue
+++ client/src/components/shared/controls/sortControl/SortControl.vue
@@ -1,6 +1,10 @@
 <template>
 	<div class="sort-control">
-		<button @click="displayList = !displayList">Sort: <span>best match <i class="fa fa-chevron-down"></i></span></button>
+		<button 
+            @click="displayList = !displayList" 
+            v-click-outside="hide" >
+            Sort: <span>best match <i class="fa fa-chevron-down"></i></span>
+        </button>
 	    <ul v-if="displayList">
             <li>A</li>
             <li>B</li>
@@ -10,20 +14,19 @@
 </template>
 
 <script>
+import ClickOutside from 'vue-click-outside';
 export default {
-	name: "SortControl",
+    name: "SortControl",
+    directives: {
+        ClickOutside
+    },
 	data: () => ({
 		displayList: false
 	}),
-	// methods:{
-	// 	emitSearch(){
-	// 		this.$emit('emit-search',this.searchTerm);
-	// 		this.searchTerm = "";
-	// 		// Listen from parent
-	// 	},
-	// 	trigger(){
-	// 		this.$refs.emitSearch.click()
-	// 	}
-	// }
+	methods:{
+		hide(){
+            this.displayList = false;
+		}
+	}
 }
 </script>
