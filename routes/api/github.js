1e697f0e8855b17245b3e290ac7e5c9bb331ee0f
milad-mirkarimi <themiladmirkarimi@gmail.com>
2020-12-06 11:15:02 +1300

    add sort param to get user api

--- routes/api/github.js
+++ routes/api/github.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const request = require('request');
 const config = require('config');
 
-// GET api/github/users/:username
+// GET api/github/users/:username/:sort
 // Get user from Github
 // @access public
-router.get('/users/:username', (req, res) => {
+router.get('/users/:username/:sort', (req, res) => {
     try {
         const options = {
-            uri: `https://api.github.com/search/users?q=${req.params.username}&per_page=10
+            uri: `https://api.github.com/search/users?q=${req.params.username}&sort=${req.params.sort}&per_page=10
             &client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
             method: 'GET',
             headers: { 'user-agent': 'node.js' }
