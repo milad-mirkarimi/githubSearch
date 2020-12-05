5f0388d5e9e46d4f81ce279d2beacda576a0e77d
milad-mirkarimi <themiladmirkarimi@gmail.com>
2020-12-06 10:24:56 +1300

    add velocity

--- client/src/components/shared/controls/sortControl/SortControl.vue
+++ client/src/components/shared/controls/sortControl/SortControl.vue
@@ -5,16 +5,23 @@
             v-click-outside="hide" >
             Sort: <span>best match <i class="fa fa-chevron-down"></i></span>
         </button>
-	    <ul v-if="displayList">
-            <li>A</li>
-            <li>B</li>
-            <li>C</li>
-        </ul>
+        <transition 
+            @before-enter="beforeEnter"
+            @enter="enter"
+            @leave="leave"
+            :css="false" >
+            <ul v-if="displayList">
+                <li>A</li>
+                <li>B</li>
+                <li>C</li>
+            </ul>
+        </transition>
 	</div>
 </template>
 
 <script>
 import ClickOutside from 'vue-click-outside';
+import Velocity from 'velocity-animate';
 export default {
     name: "SortControl",
     directives: {
@@ -26,7 +33,26 @@ export default {
 	methods:{
 		hide(){
             this.displayList = false;
-		}
-	}
+        },
+        beforeEnter(el){
+            el.style.opacity = 0;
+            el.style.height = '0px';
+        },
+        enter(el, done){
+            Velocity(
+                el,
+                { opacity: 1, height: '90px'},
+                { duration: 250, easing: [90, 10], complete: done}
+            )
+        },
+        leave(el, done){
+            Velocity(
+                el,
+                { opacity: 0, height: '0px'},
+                { duration: 200, easing: 'easeInCubic', complete: done}
+            )
+
+        }
+    }
 }
 </script>
