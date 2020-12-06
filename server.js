ccb6d91404d2204452c64f3d8fb013a3efa374d2
milad-mirkarimi <themiladmirkarimi@gmail.com>
2020-12-07 03:21:56 +1300

    minor chnages

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const path = require('path')
-const history = require('connect-history-api-fallback');
+
 
 const app = express();
-// app.use(history());
-// app.use(express.static('src'));
+
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-// app.get('/', (req, res) => {
-//     res.sendFile('src/index.html');
-// });
 
 // Define Routes Here:
 app.use('/api/github', require('./routes/api/github'));
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(__dirname + '/public'))
+    app.use(express.static(__dirname + '/public/'))
 
     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
