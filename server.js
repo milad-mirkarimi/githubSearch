869c14404e7259bfea14c6d5090d91576d8edd40
milad-mirkarimi <themiladmirkarimi@gmail.com>
2020-12-07 02:53:31 +1300

    changes

--- server.js
+++ server.js
@@ -8,20 +8,20 @@ app.use(express.static('src'));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.sendFile('src/index.html');
-});
+// app.get('/', (req, res) => {
+//     res.sendFile('src/index.html');
+// });
 
 // Define Routes Here:
 app.use('/api/github', require('./routes/api/github'));
 
-// if(process.env.NODE_ENV === 'production'){
-//     app.use(express.static('client/dist'))
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
 
-//     app.get('*', (req, res)=> {
-//         res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
-//     })
-// }
+    app.get('*', (req, res)=> {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
